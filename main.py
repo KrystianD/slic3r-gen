cbb782925e80d2ffed9f078b4a21c3305b1ac42a
Krystian Dużyński <krystian.duzynski@gmail.com>
2015-05-10 16:56:42 +0200

    added more options, added option string and bool converting

--- main.py
+++ main.py
@@ -31,10 +31,23 @@ def traverseSettings(f, v, path = ""):
 			traverseSettings(f, v, spath)
 		else:
 			processSetting(f, spath, v)
+
+def convertSetting(v):
+	if isinstance(v, str):
+		v = v.replace("\n", "\\n")
+		return v
+	if isinstance(v, bool):
+		if v:
+			return "1"
+		else:
+			return "0"
+	return v
+
 	
 def processSetting(f, k, v):
 	if k in options_map.optionsMap:
 		slicerName = options_map.optionsMap[k]
+		v = convertSetting(v)
 		f.write("{0} = {1}\n".format(slicerName, v).encode("ascii"))
 	else:
 		raise Exception("no setting " + k)
